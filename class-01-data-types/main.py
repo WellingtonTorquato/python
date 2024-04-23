54994b34ee63b1bc52e44742fc8d6c9ba156326e
Wellington Torquato <torquatojucas@gmail.com>
2024-04-23 20:36:25 -0300

    add class 01 data types

---
+++ class-01-data-types/main.py
@@ -0,0 +1,20 @@
+milk = "leite"
+volume = 2
+
+print(type(milk))
+print(type(volume))
+
+preparation1 = volume + 1
+preparation2 = milk + str(volume)
+
+print(preparation1)
+print(preparation2)
+
+life_cat = str(volume + 5)
+
+print("O gato bebe " + milk)
+print("E tem " + life_cat + " vidas")
+
+print("O gato bebe " + milk + " e tem " + life_cat + " vidas")
+
+print(f"O gato bebe {milk} e tem {life_cat} vidas")
