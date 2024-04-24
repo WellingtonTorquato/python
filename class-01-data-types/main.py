4f5b1a2bd22fae65c117fa0889a34596536c720b
Wellington Torquato <torquatojucas@gmail.com>
2024-04-24 20:30:49 -0300

    add structural types examples

--- class-01-data-types/main.py
+++ class-01-data-types/main.py
@@ -18,3 +18,56 @@ print("E tem " + life_cat + " vidas")
 print("O gato bebe " + milk + " e tem " + life_cat + " vidas")
 
 print(f"O gato bebe {milk} e tem {life_cat} vidas")
+
+name = "Torquato"
+age = 45
+is_admin = True #boolean True or False
+
+print(is_admin)
+print(type(is_admin))
+
+# Structural Types (Collections)
+
+animes = ["Drago Ball", "Naruto", "Jojo", "Death Note"]
+# posição   =>  0           1        2          3  não Null
+          # <= -4          -3       -2         -1  Null
+
+print(animes)
+print(type(animes))
+print(animes[0])
+print(animes[3])
+print(animes[-1])
+
+list = ["A", 123, True, ["B", "C"]]
+
+print(list[0])
+print(list[3])
+print(list[-1])
+print(list[3][0])
+print(list[-1][1])
+
+list[2] = False
+list[-1][0] = "D"
+print(list)
+print(len(list)) #length
+
+# tuple
+tuple = ("A", 123, True)
+print(tuple[0])
+
+#tuple[1] = 789 error:  tuple is immtable
+
+print(tuple)
+
+#dict
+dict = {
+    "name": "Torquato",
+    "age": 45,
+    "is_admin": True
+}
+print(dict)
+print(type(dict))
+
+print(dict["name"])
+print(dict["age"])
+print(dict["is_admin"])
